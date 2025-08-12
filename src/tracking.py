dab96246fef8542f2b2e7184c4a802617b6ca840
MorteZ76 <mortezamojtabavi76@gmail.com>
2025-08-12 12:14:12 +0200

    s

--- src/tracking.py
+++
@@ -1,8 +0,0 @@
-from bytetrack import BYTETracker
-
-class Tracker:
-    def __init__(self):
-        self.tracker = BYTETracker()
-
-    def update(self, detections, frame):
-        return self.tracker.update(detections, frame)
--- yolov8classes.py
+++
@@ -1,4 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO('yolov8n.pt') # yolov3-v7
-print (model.names)
